6730337634cee19cb7b70b688875013491f18c80
Rachmann Joubert <rachmannjoubert@gmail.com>
2022-04-05 17:54:39 +0100

    Defined main file

--- main.py
+++ main.py
@@ -7,11 +7,12 @@ def main():
 
     run = True
     while run:
-        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
 
     pygame.quit()
 
-            
+if __name__ == "__main__":
+    main()
+           
