ea129a5e2b9446f548d8d1218c5aa96e17a3eafb
Rachmann Joubert <rachmannjoubert@gmail.com>
2022-04-05 22:36:21 +0100

    Removed projectiles whin they hit edge of window

--- main.py
+++ main.py
@@ -74,12 +74,17 @@ def handle_projeciles(red_projectiles, yellow_projectiles, red, yellow):
         if yellow.colliderect(projectile):
             pygame.event.post(pygame.event.Event(YELLOW_HIT))
             red_projectiles.remove(projectile)
+        elif projectile.x > WIDTH:
+            red_projectiles.remove(projectile)
+
 
     for projectile in yellow_projectiles:
         projectile.x -= PROJECTILE_VEL
        
         if red.colliderect(projectile):
             pygame.event.post(pygame.event.Event(RED_HIT))
+            yellow_projectiles.remove(projectile)
+        elif projectile.x < 0:
             yellow_projectiles.remove(projectile)
 
 def main():
