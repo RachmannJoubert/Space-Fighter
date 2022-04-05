7c8028d2b14e1fd67e5b1a694f0fdb0b57be771a
Rachmann Joubert <rachmannjoubert@gmail.com>
2022-04-05 19:58:51 +0100

    Added movement for red spaceship

--- main.py
+++ main.py
@@ -7,25 +7,25 @@ WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 
 WHITE = (255, 255, 255)
 FPS = 60
+VEL = 5
 
 SPACESHIP_WIDTH, SPACESHIP_HEIGHT = 55, 45
-
-YELLOW_SPACESHIP_IMAGE = pygame.image.load(os.path.join(
-    'Assets', 'spaceship_yellow.png'))
-YELLOW_SPACESHIP = pygame.transform.rotate(pygame.transform.scale(
-    YELLOW_SPACESHIP_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT)), 270)
 
 RED_SPACESHIP_IMAGE = pygame.image.load(os.path.join(
     'Assets', 'spaceship_red.png'))
 RED_SPACESHIP = pygame.transform.rotate(pygame.transform.scale(
     RED_SPACESHIP_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT)), 90)
 
+YELLOW_SPACESHIP_IMAGE = pygame.image.load(os.path.join(
+    'Assets', 'spaceship_yellow.png'))
+YELLOW_SPACESHIP = pygame.transform.rotate(pygame.transform.scale(
+    YELLOW_SPACESHIP_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT)), 270)
 
 def draw_window(red, yellow):
     
     WIN.fill(WHITE)
-    WIN.blit(YELLOW_SPACESHIP, (yellow.x, yellow.y))
     WIN.blit(RED_SPACESHIP, (red.x, red.y))
+    WIN.blit(YELLOW_SPACESHIP, (yellow.x, yellow.y))
     pygame.display.update()
 
 
@@ -42,8 +42,17 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-        
-        yellow.x += 1
+
+        keys_pressed = pygame.key.get_pressed()
+        if keys_pressed[pygame.K_a]: # LEFT
+            red.x -= VEL
+        if keys_pressed[pygame.K_d]: # RIGHT
+            red.x += VEL
+        if keys_pressed[pygame.K_w]: # UP
+            red.y -= VEL
+        if keys_pressed[pygame.K_s]: # DOWN
+            red.y += VEL
+         
         draw_window(red, yellow)
 
     pygame.quit()
