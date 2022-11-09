231139ff9d61269521cba61cea649e6daba7ccb5
Rachmann Joubert <rachmannjoubert@gmail.com>
2022-11-09 20:01:22 +0000

    Imported necessary modules to winner.py

--- winner.py
+++ winner.py
@@ -1,3 +1,6 @@
+import pygame
+from global_var import *
+
 def winner(text):
     draw_text = WINNER_FONT.render(text, 1, WHITE)
     WIN.blit(draw_text, (WIDTH/2 - draw_text.get_width()/2, HEIGHT/2 - draw_text.get_height()/2))
